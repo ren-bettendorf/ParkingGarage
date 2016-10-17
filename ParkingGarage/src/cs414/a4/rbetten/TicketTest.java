69a4efd0b28f659a43f19ac8bb09b939ce64a36d
Ren Bettendorf <Ren Bettendorf>
2016-10-17 12:44:02 -0700

    Initial commit for assignment 4

---
+++ ParkingGarage/src/cs414/a4/rbetten/TicketTest.java
@@ -0,0 +1,19 @@
+package cs414.a4.rbetten;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TicketTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
