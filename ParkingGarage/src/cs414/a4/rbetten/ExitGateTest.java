23545af1da4aaf5f8c87a12750ebe2a9febb3cee
Ren Bettendorf <rbetten@Tannas-MacBook-Pro.local>
2016-10-20 20:13:34 -0700

    Added rest of initial classes and tests

---
+++ ParkingGarage/src/cs414/a4/rbetten/ExitGateTest.java
@@ -0,0 +1,19 @@
+package cs414.a4.rbetten;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExitGateTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
