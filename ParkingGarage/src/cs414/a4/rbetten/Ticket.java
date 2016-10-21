a6c96f9130a611dbd1457c697a745748c61c67b9
Ren Bettendorf <rbetten@Tannas-MacBook-Pro.local>
2016-10-20 19:41:21 -0700

    Added initial Ticket and TicketTest classes

--- ParkingGarage/src/cs414/a4/rbetten/Ticket.java
+++ ParkingGarage/src/cs414/a4/rbetten/Ticket.java
@@ -1,5 +1,53 @@
 package cs414.a4.rbetten;
 
-public class Ticket {
+import java.time.LocalDateTime;
 
+public class Ticket {
+	
+	private LocalDateTime checkinTime;
+	private LocalDateTime checkoutTime;
+	private boolean paymentStatus = false;
+	
+	
+	public Ticket(LocalDateTime checkinTime)
+	{
+		this.checkinTime = checkinTime;
+	}
+	
+	public LocalDateTime getCheckinTime()
+	{
+		return checkinTime;
+	}
+	
+	public void setCheckoutTime(LocalDateTime ldt)
+	{
+		if(checkinTime.compareTo(ldt) < 0)
+		{
+			this.checkoutTime = ldt;
+		}
+	}
+	
+	public void setPaymentStatus(boolean status)
+	{
+		this.paymentStatus = status;
+	}
+	
+	public boolean getPaymentStatus()
+	{
+		return this.paymentStatus;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Ticket) {
+			return checkinTime.equals(((Ticket) obj).getCheckinTime());
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return checkinTime.toString();
+	}
 }
--- ParkingGarage/src/cs414/a4/rbetten/TicketTest.java
+++ ParkingGarage/src/cs414/a4/rbetten/TicketTest.java
@@ -2,18 +2,36 @@ package cs414.a4.rbetten;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDateTime;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TicketTest {
 
+	Ticket ticket1;
+	Ticket ticket2;
+	Ticket ticket3;
+	
 	@Before
 	public void setUp() throws Exception {
+		LocalDateTime ldt = LocalDateTime.now();
+		
+		ticket1 = new Ticket(ldt);
+		ticket2 = new Ticket(ldt);
+		
+		ticket3 = new Ticket(LocalDateTime.now());
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void equalTickets() {
+		Assert.assertTrue(ticket1.equals(ticket2));
+	}
+	
+	@Test
+	public void notEqualTickets() {
+		Assert.assertFalse(ticket1.equals(ticket3));
 	}
 
 }
